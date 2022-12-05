688de723c2b5ff9c83d0afb0cfeced6290926534
Yahdiha72 <94019411+Yahdiha72@users.noreply.github.com>
2022-12-05 20:18:42 +0100

    Add files via upload

---
+++ demo/src/main/java/com/springboot/demo/PfileApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfileApplication.class, args);
+	}
+
+}
